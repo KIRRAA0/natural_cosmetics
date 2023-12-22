77f36d6af41b4fa52a0c0fe20c73478de402f9ef
Ali-Ahmed <kirraa699@gmail.com>
2023-12-22 14:41:53 +0200

    done6

--- lib/Model/colors.dart
+++ lib/Model/colors.dart
@@ -4,5 +4,6 @@ class Colors {
   static const primaryColor = Color(0xFF0C9869);
   static const textColor = Color(0xFF3C4046);
   static const backgroundColor = Color(0xFFF9F8FD);
+  static const backgroundColor3 = Color(0xFFF9F8FD);
   static const backgroundColor2 = Color(0xFFF9F8FD);
 }
